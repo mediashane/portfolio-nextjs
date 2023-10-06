4dc9a08104f800f46e4820742281796642df88f4
mediashane <shane@mediashane.com>
2023-10-06 07:38:17 +0100

    speech Link

--- pages/speech.tsx
+++ pages/speech.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import React from "react";
+import Link from 'next/link'
 
 import { Layout } from "../src/components/layout/Layout";
 import { Projects } from "../src/components/projects/Projects";
@@ -10,7 +11,11 @@ const Home: NextPage = () => {
     return (
         <>
             <Layout>
-                <a href="https://docs.google.com/presentation/d/1FSQ6CBMgLJt4U6HIGLBC3ycSS8aFCIHZqgl1yBjdPUQ/edit?usp=sharing">Speech</a>
+                <Link passHref href="https://docs.google.com/presentation/d/1FSQ6CBMgLJt4U6HIGLBC3ycSS8aFCIHZqgl1yBjdPUQ/edit?usp=sharing">
+                    <a>
+                        Speech
+                    </a>
+                </Link>
             </Layout>
         </>
     );

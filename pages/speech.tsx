84b148a2a4b2348c0b33b9abc73a34c8ae32dd60
mediashane <shane@mediashane.com>
2023-10-06 07:46:18 +0100

    speech Link 2

--- pages/speech.tsx
+++ pages/speech.tsx
@@ -11,11 +11,13 @@ const Home: NextPage = () => {
     return (
         <>
             <Layout>
-                <Link passHref href="https://docs.google.com/presentation/d/1FSQ6CBMgLJt4U6HIGLBC3ycSS8aFCIHZqgl1yBjdPUQ/edit?usp=sharing">
-                    <a>
-                        Speech
-                    </a>
-                </Link>
+                <PageTransition>
+                    <Link passHref href="https://docs.google.com/presentation/d/1FSQ6CBMgLJt4U6HIGLBC3ycSS8aFCIHZqgl1yBjdPUQ/edit?usp=sharing">
+                        <a>
+                            Speech
+                        </a>
+                    </Link>
+                </PageTransition>
             </Layout>
         </>
     );
